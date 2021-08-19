dd8ad087f8c7ed4f4c709fb3fc239bf85eb5e369
John Leskas <john.leskas@gmail.com>
2021-08-19 15:23:56 +0300

    :sparkles: Product macthes panel field improvements

--- src/editor-panels/product-matches-panel/index.js
+++ src/editor-panels/product-matches-panel/index.js
@@ -9,49 +9,90 @@ import {Button, IconButton, PanelBody} from "@wordpress/components";
  */
 import ProductAddonsSelect from "../../editor-components/product-addons-select";
 import ProductCategorySelect from "../../editor-components/product-category-select";
+import {getRandomInt, moveInArray} from "../../utils/index";
+import {ReactSortable} from "react-sortablejs";
+
 
 class ProductMatchesPanel extends Component {
 
     handleAddProductMatches = () => {
-        const productMatchesItems = [...this.props.attributes.productMatchesItems];
-        productMatchesItems.push({id: ''});
-        this.props.setAttributes({productMatchesItems});
+        const randomNum = getRandomInt(1, 1000);
+
+        const productMatches = [...this.props.attributes.productMatches];
+        productMatches.push({id: randomNum});
+        this.props.setAttributes({productMatches});
     };
 
     handleRemoveProductMatches = (index) => {
-        const productMatchesItems = [...this.props.attributes.productMatchesItems];
-        productMatchesItems.splice(index, 1);
-        this.props.setAttributes({productMatchesItems});
+        const productMatches = [...this.props.attributes.productMatches];
+        productMatches.splice(index, 1);
+        this.props.setAttributes({productMatches});
+
+        if (productMatches.length === 0) {
+            this.props.setAttributes({productMatchesSelectedCategory: []})
+            this.props.setAttributes({productMatchesProductAddons: []})
+        }
     };
+
+    handleSortProductMatches = (newState) => {
+        this.props.setAttributes({productMatches: newState})
+    }
+
+    handleOnEndSort = (evt) => {
+        const oldIndex = evt.oldIndex;
+        const newIndex = evt.newIndex;
+
+        if (this.props.attributes.productMatchesProductAddons.length > 0) {
+            const productMatchesProductAddons = [...this.props.attributes.productMatchesProductAddons];
+            moveInArray(productMatchesProductAddons, oldIndex, newIndex);
+            this.props.setAttributes({productMatchesProductAddons})
+        }
+
+        if (this.props.attributes.productMatchesSelectedCategory.length > 0) {
+            const productMatchesSelectedCategory = [...this.props.attributes.productMatchesSelectedCategory];
+            moveInArray(productMatchesSelectedCategory, oldIndex, newIndex);
+            this.props.setAttributes({productMatchesSelectedCategory})
+        }
+    }
 
     render() {
         let productMatchesFields;
         const {attributes} = this.props;
         const {
-            productMatchesItems,
+            productMatches,
         } = attributes;
 
-        if (productMatchesItems.length) {
-            productMatchesFields = productMatchesItems.map((location, index) => {
+        if (productMatches.length) {
+            productMatchesFields = attributes.productMatches.map((item, index) => {
                 return (
-                    <>
-                        <ProductCategorySelect {...this.props} index={index}/>
+                    <div key={item.id} style={{cursor: 'grab'}}>
 
-                        <ProductAddonsSelect {...this.props} index={index} label={__('Select Product Add-ons:')}/>
+                        <ProductCategorySelect {...this.props} index={index} productMatchesSelect />
+
+                        <ProductAddonsSelect {...this.props} index={index} label={__('Select Product Add-ons:')} productMatchesSelect />
 
                         <IconButton
                             icon="no-alt"
                             label="Delete product match"
                             onClick={() => this.handleRemoveProductMatches(index)}
                         />
-                    </>
+
+                    </div>
                 );
             });
         }
 
+        const sortableList = <ReactSortable
+            list={attributes.productMatches}
+            setList={(newState) => this.handleSortProductMatches(newState)}
+            onEnd={(evt) => this.handleOnEndSort(evt)}
+        >
+            {productMatchesFields}
+        </ReactSortable>
+
         return (
             <PanelBody title={__('Product Matches')}>
-                {productMatchesFields}
+                {sortableList}
                 <Button
                     isPrimary
                     onClick={this.handleAddProductMatches.bind(this)}
